c898fb70f49eb3b836fc794e4957ca6d1fa24db8
Ullaakut <brendan.le-glaunec@epitech.eu>
2018-12-11 06:03:03 -0800

    Day11 Part01 & Part02: Done & tests added

--- day11/1/main.go
+++ day11/1/main.go
@@ -1,27 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 )
 
-const inputFilePath = "input.txt"
+type coord struct {
+	x int
+	y int
+}
 
-func computeXXX(content []byte) int {
-	return 0
+type cells map[coord]int
+
+func bestSubgrid(grid cells) (int, int) {
+	bestX, bestY, bestTotalPower := 0, 0, 0
+
+	for x := 0; x < 298; x++ {
+		for y := 0; y < 298; y++ {
+			totalPower := grid[coord{x, y}] + grid[coord{x + 1, y}] + grid[coord{x + 2, y}]
+			totalPower += grid[coord{x, y + 1}] + grid[coord{x + 1, y + 1}] + grid[coord{x + 2, y + 1}]
+			totalPower += grid[coord{x, y + 2}] + grid[coord{x + 1, y + 2}] + grid[coord{x + 2, y + 2}]
+
+			if totalPower > bestTotalPower {
+				bestTotalPower = totalPower
+				bestX = x
+				bestY = y
+			}
+		}
+	}
+
+	log.Printf("Found best X,Y at %d,%d with a total of %d power", bestX, bestY, bestTotalPower)
+	return bestX, bestY
+}
+
+func solveExercise(serialNumber int) (int, int) {
+	grid := make(cells)
+
+	for x := 0; x < 300; x++ {
+		for y := 0; y < 300; y++ {
+			grid[coord{x, y}] = (((((x+10)*y)+serialNumber)*(x+10)/100)%10 - 5)
+		}
+	}
+
+	return bestSubgrid(grid)
 }
 
 func main() {
 	log.Println("Beginning day11ex01...")
 
-	contents, err := ioutil.ReadFile(inputFilePath)
-	if err != nil {
-		log.Fatal(fmt.Sprint("Unable to read input file:", err))
-	}
+	x, y := solveExercise(9306)
 
-	XXX := computeXXX(contents)
-
-	log.Println("XXX successfully computed")
-	log.Printf("XXX: %d\n", XXX)
+	log.Printf("Cell grid computed")
+	log.Printf("Best 3x3 cell grid: %d,%d", x, y)
 }
--- day11/2/main.go
+++ day11/2/main.go
@@ -1,27 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 )
 
-const inputFilePath = "input.txt"
+type coord struct {
+	x int
+	y int
+}
 
-func computeXXX(content []byte) int {
-	return 0
+type cells [301][301]int
+
+func bestSubgrid(grid cells) (int, int, int) {
+	bestX, bestY, bestSize, bestTotalPower := 0, 0, 0, 0
+
+	for s := 1; s <= 300; s++ {
+		for x := s; x <= 300; x++ {
+			for y := s; y <= 300; y++ {
+				total := grid[x][y] - grid[x-s][y] - grid[x][y-s] + grid[x-s][y-s]
+
+				if total > bestTotalPower {
+					bestTotalPower = total
+					bestX = x
+					bestY = y
+					bestSize = s
+				}
+			}
+		}
+	}
+
+	return bestX - bestSize + 1, bestY - bestSize + 1, bestSize
+}
+
+func solveExercise(serialNumber int) (int, int, int) {
+	grid := cells{}
+
+	for x := 1; x <= 300; x++ {
+		for y := 1; y <= 300; y++ {
+			power := (((((x+10)*y)+serialNumber)*(x+10)/100)%10 - 5)
+			grid[x][y] = power + grid[x-1][y] + grid[x][y-1] - grid[x-1][y-1]
+		}
+	}
+
+	return bestSubgrid(grid)
 }
 
 func main() {
 	log.Println("Beginning day11ex02...")
 
-	contents, err := ioutil.ReadFile(inputFilePath)
-	if err != nil {
-		log.Fatal(fmt.Sprint("Unable to read input file:", err))
-	}
+	x, y, size := solveExercise(9306)
 
-	XXX := computeXXX(contents)
-
-	log.Println("XXX successfully computed")
-	log.Printf("XXX: %d\n", XXX)
+	log.Printf("Cell grid computed")
+	log.Printf("Best cell grid: %d,%d,%d", x, y, size)
 }
